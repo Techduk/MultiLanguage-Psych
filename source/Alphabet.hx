7cd0dd7b9e1f2403822d83606a323679267dd472
__Funduk__ <72240167+Techduk@users.noreply.github.com>
2022-08-20 19:47:00 +0700

    Update Alphabet

--- source/Alphabet.hx
+++ source/Alphabet.hx
@@ -372,7 +372,7 @@ class Alphabet extends FlxSpriteGroup
 
 class AlphaCharacter extends FlxSprite
 {
-	public static var alphabet:String = "abcdefghijklmnopqrstuvwxyz";
+	public static var alphabet:String = "abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 
 	public static var numbers:String = "1234567890";
 
